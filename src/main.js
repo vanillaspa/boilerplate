0be9f195aaa590b1460dab766dcc6e85d6060acb
J4H20M3 <rome@live.com>
2026-02-22 03:26:13 +0100

    added: boilerplate

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import('@vanillaspa/web-components');
+
+customElements.whenDefined("router-app").then(() => {
+    Promise.all([
+        import('@vanillaspa/event-bus'),
+        import('@vanillaspa/sqlite-database')
+    ]).then((importedModules) => {
+        importedModules.forEach((module) => {
+            if (!module.name) {
+                throw new Error('Missing name in imported module.');
+            }
+            window[module.name] = module;
+        });
+    }).finally(() => {
+        const root = document.getElementsByTagName('body')[0];
+        root.innerHTML = '<router-app></router-app>';
+    });
+})
