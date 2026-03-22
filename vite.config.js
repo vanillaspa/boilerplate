7b11bd108ad0db159a924dbd7f3e474e16e48c34
J4H20M3 <rome@live.com>
2026-03-23 00:59:37 +0100

    test: bus

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,12 @@ export default defineConfig({
         strictPort: true,
         headers: {
             'Cross-Origin-Opener-Policy': 'same-origin', // for sqlite OPFS
-            'Cross-Origin-Embedder-Policy': 'require-corp' // for sqlite OPFS
+            'Cross-Origin-Embedder-Policy': 'require-corp', // for sqlite OPFS
+            // 'Content-Security-Policy':
+            //     "default-src 'unsafe-eval'; script-src 'self' 'unsafe-eval'; object-src 'none'; base-uri 'self';",
+            'X-Frame-Options': 'DENY',
+            'X-Content-Type-Options': 'nosniff',
+            'Referrer-Policy': 'strict-origin-when-cross-origin',
         },
         hmr: {
             host: 'localhost',
